963235fdc4f32eb33f14f9a8b4aec5442ffacaab
americanman <snowboardracer7@gmail.com>
2019-03-17 15:49:02 -0400

    Compare performance

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-//const chunkArrayInGroups = (arr, size) => {
-//  let structuredArr = []
-//  let classifier = arr.length % size
-//  let equalParts = Math.floor(arr.length / size)
-//
-//  for (let i = 0; i < arr.length; i += size) {
-//    let newArr = [];
-//    if (classifier != 0) {
-//      if (structuredArr.length == equalParts) {
-//        size = classifier
-//      }
-//    }
-//    for (let y = 0; y < size; y++) {
-//      newArr.push(arr[i + y])
-//    }
-//    structuredArr.push(newArr)
-//  }
-//
-//
-//  return structuredArr;
-//}
+const {
+  performance
+} = require('perf_hooks');
 
-// second solution with slice method
-// slice takes two arguents and if end is greater 
-// then length it just slice till the last element of the array
+var t0 = performance.now();
 
-
-const chunkArrayInGroups = (arr, size) => {
+function chunkArrayInGroups(arr, size) {
   let structuredArr = []
-  for(let i = 0; i < arr.length; i+=size) {
+
+  for (let i = 0; i < arr.length; i += size)
     structuredArr.push(arr.slice(i, i + size))
+  return structuredArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d", "f"], 2); 
+
+var t1 = performance.now();
+console.log("slice took " + (t1 - t0) + " milliseconds.")
+
+
+
+var t0 = performance.now();
+
+function chunkBank(arr, size) {
+  let structuredArr = []
+  let classifier = arr.length % size
+  let equalParts = Math.floor(arr.length / size)
+
+  for (let i = 0; i < arr.length; i += size) {
+    let newArr = [];
+    if (classifier != 0) {
+      if (structuredArr.length == equalParts) {
+        size = classifier
+      }
+    }
+    for (let y = 0; y < size; y++) {
+      newArr.push(arr[i + y])
+    }
+    structuredArr.push(newArr)
   }
   return structuredArr;
 }
-console.log(chunkArrayInGroups(["a", "b", "c", "d", "f"], 2))
+
+
+chunkBank(["a", "b", "c", "d", "f"], 2); 
+var t1 = performance.now();
+console.log("original took " + (t1 - t0) + " milliseconds.")
